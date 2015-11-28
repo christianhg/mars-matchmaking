fd531a290b3c1769672c236088e8c75c46300910
Christian Grøngaard <christianhg@gmail.com>
2015-11-28 09:49:44 +0100

    removes unused step2 controller

--- src/app/register/step2/register.step2.config.js
+++ src/app/register/step2/register.step2.config.js
@@ -9,12 +9,7 @@
       $stateProvider
           .state('register.step2', {
               url: '/2',
-              templateUrl: 'register.step2.view.html',
-              // controller: 'RegisterStep2Controller',
-              // controllerAs: 'vm',
-              // data: {
-              //     pageTitle: 'Mars Matchmaking > Register > 2'
-              // }
+              templateUrl: 'register.step2.view.html'
           });
   }
 })();
