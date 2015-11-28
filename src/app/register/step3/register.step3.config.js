0401569f7898332f7c156252d6c63a09f2577bb8
Christian Grøngaard <christianhg@gmail.com>
2015-11-28 09:50:08 +0100

    removes unused step3 controller

--- src/app/register/step3/register.step3.config.js
+++ src/app/register/step3/register.step3.config.js
@@ -9,12 +9,7 @@
       $stateProvider
           .state('register.step3', {
               url: '/3',
-              templateUrl: 'register.step3.view.html',
-              // controller: 'RegisterStep3Controller',
-              // controllerAs: 'vm',
-              // data: {
-              //     pageTitle: 'Mars Matchmaking > Register > 3'
-              // }
+              templateUrl: 'register.step3.view.html'
           });
   }
 })();
